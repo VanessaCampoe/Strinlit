799928d5954f76d61576ab47b9d715c3aae284de
vanessa <vanessacampoe@gmail.com>
2024-10-09 12:01:46 -0300

    09/10/2024

---
+++ aula03.py
@@ -0,0 +1,9 @@
+import streamlit as st 
+import pandas as pd 
+st.title("Carregue o seu dataSet")
+
+upload = st.file_uploader("escolha o arquivo ", type="csv")
+
+if upload is not None:
+    df = pd.read_csv(upload)
+    st.dataframe(df)
